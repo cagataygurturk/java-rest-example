7e5c8225fef2c29582b6b40f7386bf999ae579d1
cagataygurturk <cagatay@itusozluk.com>
2016-01-13 00:19:43 +0100

    Refactor field name

---
+++ src/main/java/com/cagataygurturk/models/Transaction.java
@@ -0,0 +1,82 @@
+package com.cagataygurturk.models;
+
+import com.cagataygurturk.storage.Storable;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+
+public class Transaction implements Serializable, Storable {
+
+    protected long id;
+
+    protected double amount;
+
+    protected String type;
+
+    protected long parentId;
+
+    public Transaction(double amount, String type) {
+        this.amount = amount;
+        this.type = type;
+    }
+
+    public Transaction(double amount, String type, Transaction parent) {
+        this(amount, type);
+        this.parentId = parent.getId();
+    }
+
+    @JsonProperty("transaction_id")
+    public long getId() {
+        return id;
+    }
+
+    public Transaction setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    @JsonProperty("amount")
+    public double getAmount() {
+        return amount;
+    }
+
+    public Transaction setAmount(double amount) {
+        this.amount = amount;
+        return this;
+    }
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    public Transaction setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    @JsonProperty("parent_id")
+    public Long getParentId() {
+
+        if (parentId == 0) {
+            return null;
+        }
+
+        return parentId;
+    }
+
+
+    public Transaction setParent(Transaction parent) {
+        this.parentId = parent.getId();
+        return this;
+    }
+
+    public long getGeneratedId() {
+        return this.getId();
+    }
+
+    public void setGeneratedId(long id) {
+        this.id = id;
+    }
+}
---
+++ src/test/java/com/cagataygurturk/models/TransactionTest.java
@@ -0,0 +1,69 @@
+package com.cagataygurturk.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransactionTest {
+
+    /**
+     * Extremely small number for double-double comparison
+     */
+    protected static final double DELTA = 1e-5;
+
+    protected Transaction getTransactionObject() {
+        return new Transaction(20.0, "car");
+    }
+
+    @Test
+    public void testGetTransactionId() throws Exception {
+        assertEquals(1, getTransactionObject().getId());
+    }
+
+    @Test
+    public void testSetTransactionId() throws Exception {
+        long newId = 30;
+        Transaction transaction = getTransactionObject();
+        transaction.setId(newId);
+        assertEquals(newId, transaction.getId());
+    }
+
+    @Test
+    public void testGetAmount() throws Exception {
+        assertEquals(20.0, getTransactionObject().getAmount(), DELTA);
+    }
+
+    @Test
+    public void testSetAmount() throws Exception {
+        double newAmount = 30.0;
+        Transaction transaction = getTransactionObject();
+        transaction.setAmount(newAmount);
+        assertEquals(newAmount, transaction.getAmount(), DELTA);
+
+    }
+
+    @Test
+    public void testGetType() throws Exception {
+        assertEquals("car", getTransactionObject().getType());
+    }
+
+    @Test
+    public void testSetType() throws Exception {
+        String newType = "food";
+        Transaction transaction = getTransactionObject();
+        transaction.setType(newType);
+        assertEquals(newType, transaction.getType());
+    }
+
+    @Test
+    public void testGetParentId() throws Exception {
+        Transaction transaction = getTransactionObject();
+        assertNull(transaction.getParentId());
+        Transaction parentTransaction = new Transaction(20.0, "bus");
+        parentTransaction.setId(2);
+        transaction.setParent(parentTransaction);
+        assertEquals(2, transaction.getParentId().longValue());
+    }
+
+
+}
